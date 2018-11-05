3ea26cf35e1c0aaf77dcfa4808701bcb772fe72b
CollinFields <35911424+CollinFields@users.noreply.github.com>
2018-11-05 12:59:45 -0500

    Update reverseString.py

--- TextProjects/reverseString.py
+++ TextProjects/reverseString.py
@@ -1,3 +1,7 @@
+#Programmer: Collin M. Fields
+#Date: 11/05/2018
+#Purpose: Function to reverse a function in python.
+
 def reverseString(toReverse):
 	if(isinstance(toReverse, str)):
 		return toReverse[::-1]
