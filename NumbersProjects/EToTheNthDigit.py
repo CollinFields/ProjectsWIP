461a3c1b4f4a6a55f6f3b2efa69b887359489edc
CollinFields <35911424+CollinFields@users.noreply.github.com>
2018-11-07 12:41:07 -0500

    Create EToTheNthDigit.py

---
+++ NumbersProjects/EToTheNthDigit.py
@@ -0,0 +1,23 @@
+"""
+	Programmer: Collin Michael Fields
+	Date: 11/1/2018
+	Purpose: Calculate the value of Pi out to a certain decimal place. (Currently only works to the 48th decimal place.
+"""
+import math
+from decimal import *
+
+#Setting the precision to a value that will not cause it to error out.
+getcontext().prec = 999
+
+print("Welcome to the E calculator.\nPlease enter a value to calculate e to that decimal place (Our current limit is 48).")
+
+while(1 == 1):
+	userInput = int(input())
+	if(userInput > -1 and userInput < 49):
+		break
+	else:
+		print("You have entered an invalid value. Please re-enter a valid number")
+
+valueToBeDisplayed = round(Decimal(math.e), userInput)
+
+print(valueToBeDisplayed)
