6835c5c92f02026846fdcbb629923ef3a66947f1
CollinFields <35911424+CollinFields@users.noreply.github.com>
2018-11-08 12:18:20 -0500

    Create sieveOfEratosthenes.py

---
+++ ClassicAlgorithms/sieveOfEratosthenes.py
@@ -0,0 +1,15 @@
+#Programmer: Collin M. Fields
+#Date: 11/08/2018
+#Purpose: Create the Sieve of Eratosthenes
+
+import math
+
+def sieveOfEratosthenes(n):
+	listOfNotPrimes = []
+	listOfPrimes = []
+	for i in range(2, n+1):
+		if i not in listOfNotPrimes:
+			listOfPrimes.append(i)
+			for j in range(i*i, n+1, i):
+				listOfNotPrimes.append(j)
+	return listOfPrimes
